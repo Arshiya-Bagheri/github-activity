270317774a6aff09318e67422b7da855e442d5ac
Arshiya Bagheri <arshiyabagheri7@gmail.com>
2026-08-18 17:27:32 +0330

    Fix argparse input

--- github_activity/handler.py
+++ github_activity/handler.py
@@ -43,3 +43,39 @@ class EventHandler:
 
     def handle_unknown(self, event):
         return f"Unsupported event: {event['type']}"
+
+    def handle_PullRequestEvent(self, event):
+        payload = event["payload"]
+
+        action = payload["action"]
+        number = payload["number"]
+        repo = event["repo"]["name"]
+
+        return f"Pull request #{number} {action} in {repo}"
+
+    def handle_IssuesEvent(self, event):
+        payload = event["payload"]
+
+        action = payload["action"]
+        number = payload["issue"]["number"]
+        title = payload["issue"]["title"]
+        repo = event["repo"]["name"]
+
+        return f"Issue #{number} {action} in {repo}: {title}"
+
+    def handle_WatchEvent(self, event):
+        repo = event["repo"]["name"]
+
+        return f"Starred {repo}"
+
+    def handle_PublicEvent(self, event):
+        repo = event["repo"]["name"]
+
+        return f"Made {repo} public"
+
+    def handle_ReleaseEvent(self, event):
+        repo = event["repo"]["name"]
+        action = event["payload"]["action"]
+        tag = event["payload"]["release"]["tag_name"]
+
+        return f"Release {tag} {action} in {repo}"
--- main.py
+++ main.py
@@ -1,14 +1,29 @@
 """Command-line interface for the weather application."""
 
+import argparse
+
 from github_activity.api import GitHubAPI
 from github_activity.handler import EventHandler
 
 
 def main():
-    username = "xai"
+
+    parser = argparse.ArgumentParser(
+        prog="Github-Activity API",
+        description="Get the recent GitHub activity of a user."
+    )
+
+    parser.add_argument(
+        "username",
+        help="GitHub username to fetch activity for"
+    )
+
+    args = parser.parse_args()
 
     api = GitHubAPI()
-    events = api.get_user_events(username)
+
+    events = api.get_user_events(args.username)
+
     handler = EventHandler()
 
     for event in events:
@@ -19,6 +34,3 @@ def main():
 if __name__ == "__main__":
     main()
 
-
-if __name__ == "__main__":
-    main()
